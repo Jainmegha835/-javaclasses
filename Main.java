37e9b95ff014d43c301f863af582ff7f6e605af2
megha <43913641+Jainmegha835@users.noreply.github.com>
2019-09-10 11:21:29 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner s1= new Scanner(System.in);
+        System.out.println("enter height=");
+        int a1=s1.nextInt();
+        System.out.println("enter base=");
+        int b1=s1.nextInt();
+        float area;
+        area=0.5f*a1*b1;
+        System.out.println("area is="+ area);
+
+    }
+}
